eb3f8e798afaf466da583ca8225196201e6ec8ff
Felipe Henrique dos Santos Borba <felipe.hborba@hotmail.com>
2023-02-05 20:06:00 -0300

    Inserting pages and routes.
    Inserting the Navbar,, some components and Routes.

---
+++ miniblog/src/firebase/config.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALXbrVLqE0TQ7BsX3uvWoHob6-hniAWhY",
+  authDomain: "miniblog-26d37.firebaseapp.com",
+  projectId: "miniblog-26d37",
+  storageBucket: "miniblog-26d37.appspot.com",
+  messagingSenderId: "1005622485986",
+  appId: "1:1005622485986:web:61ecb6cbed4715eb554fbb"
+};
+
+// inicializamos o banco que salvará os dados:
+
+const db = getFirestore(app)
+
+//aqui exportamos o banco de dados para usarmos em outro lugar:
+
+export { db };
---
+++ miniblog/src/pages/Register/Register.js
@@ -0,0 +1,11 @@
+import styles from './Register.modules.css';
+
+const Register = () => {
+  return (
+    <div>
+        <h1>Cadastrar</h1>
+    </div>
+  )
+}
+
+export default Register
