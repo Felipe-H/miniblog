36d9c790c4e1a2841552a2c3a8f0782b11185369
Felipe Henrique dos Santos Borba <felipe.hborba@hotmail.com>
2023-02-05 21:57:48 -0300

    Creating form
    Creating the registration form

--- miniblog/src/pages/Register/Register.js
+++ miniblog/src/pages/Register/Register.js
@@ -1,11 +1,53 @@
-import styles from './Register.modules.css';
+import styles from "./Register.modules.css";
+
+import { useState, useEffect } from "react";
 
 const Register = () => {
   return (
     <div>
-        <h1>Cadastrar</h1>
+      <h1>Cadastre-se para postar.</h1>
+      <p>Crie seu usuário e compartilhe suas histórias.</p>
+      <form>
+        <label>
+          <span>Nome: </span>
+          <input
+            type="text"
+            name="displayName"
+            required
+            placeholder="Nome do usuário"
+          />
+        </label>
+        <label>
+          <span>E-mail: </span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="E-mail do usuário"
+          />
+        </label>
+        <label>
+          <span>Senha: </span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira sua senha"
+          />
+        </label>
+        <label>
+          <span>Confirme sua senha: </span>
+          <input
+            type="password"
+            name="confirmPassword"
+            required
+            placeholder="Confirme a sua senha"
+          />
+        </label>
+        <button className="btn">Cadastrar</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
